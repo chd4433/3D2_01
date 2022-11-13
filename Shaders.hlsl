3bb4fcaf56dd28300bef7ec735965aa56a908b22
chd4433 <chdkhj4433@naver.com>
2022-11-14 01:42:40 +0900

    초기 커밋.

---
+++ Shaders.hlsl
@@ -0,0 +1,486 @@
+struct MATERIAL
+{
+	float4					m_cAmbient;
+	float4					m_cDiffuse;
+	float4					m_cSpecular; //a = power
+	float4					m_cEmissive;
+};
+
+cbuffer cbCameraInfo : register(b1)
+{
+	matrix		gmtxView : packoffset(c0);
+	matrix		gmtxProjection : packoffset(c4);
+	float3		gvCameraPosition : packoffset(c8);
+};
+
+cbuffer cbGameObjectInfo : register(b2)
+{
+	matrix		gmtxGameObject : packoffset(c0);
+	MATERIAL	gMaterial : packoffset(c4);
+	uint		gnTexturesMask : packoffset(c8);
+};
+
+cbuffer cbFrameworkInfo : register(b3)
+{
+	float 		gfCurrentTime;
+	float		gfElapsedTime;
+	float2		gf2CursorPos;
+};
+
+cbuffer cbWaterInfo : register(b5)
+{
+	matrix		gf4x4TextureAnimation : packoffset(c0);
+};
+
+#include "Light.hlsl"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//#define _WITH_VERTEX_LIGHTING
+
+#define MATERIAL_ALBEDO_MAP			0x01
+#define MATERIAL_SPECULAR_MAP		0x02
+#define MATERIAL_NORMAL_MAP			0x04
+#define MATERIAL_METALLIC_MAP		0x08
+#define MATERIAL_EMISSION_MAP		0x10
+#define MATERIAL_DETAIL_ALBEDO_MAP	0x20
+#define MATERIAL_DETAIL_NORMAL_MAP	0x40
+
+#define _WITH_STANDARD_TEXTURE_MULTIPLE_DESCRIPTORS
+
+#ifdef _WITH_STANDARD_TEXTURE_MULTIPLE_DESCRIPTORS
+Texture2D gtxtAlbedoTexture : register(t6);
+Texture2D gtxtSpecularTexture : register(t7);
+Texture2D gtxtNormalTexture : register(t8);
+Texture2D gtxtMetallicTexture : register(t9);
+Texture2D gtxtEmissionTexture : register(t10);
+Texture2D gtxtDetailAlbedoTexture : register(t11);
+Texture2D gtxtDetailNormalTexture : register(t12);
+#else
+Texture2D gtxtStandardTextures[7] : register(t6);
+#endif
+
+SamplerState gssWrap : register(s0);
+
+struct VS_STANDARD_INPUT
+{
+	float3 position : POSITION;
+	float2 uv : TEXCOORD;
+	float3 normal : NORMAL;
+	float3 tangent : TANGENT;
+	float3 bitangent : BITANGENT;
+};
+
+struct VS_STANDARD_OUTPUT
+{
+	float4 position : SV_POSITION;
+	float3 positionW : POSITION;
+	float3 normalW : NORMAL;
+	float3 tangentW : TANGENT;
+	float3 bitangentW : BITANGENT;
+	float2 uv : TEXCOORD;
+};
+
+VS_STANDARD_OUTPUT VSStandard(VS_STANDARD_INPUT input)
+{
+	VS_STANDARD_OUTPUT output;
+
+	output.positionW = (float3)mul(float4(input.position, 1.0f), gmtxGameObject);
+	output.normalW = mul(input.normal, (float3x3)gmtxGameObject);
+	output.tangentW = (float3)mul(float4(input.tangent, 1.0f), gmtxGameObject);
+	output.bitangentW = (float3)mul(float4(input.bitangent, 1.0f), gmtxGameObject);
+	output.position = mul(mul(float4(output.positionW, 1.0f), gmtxView), gmtxProjection);
+	output.uv = input.uv;
+
+	return(output);
+}
+
+float4 PSStandard(VS_STANDARD_OUTPUT input) : SV_TARGET
+{
+	float4 cAlbedoColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+	float4 cSpecularColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+	float4 cNormalColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+	float4 cMetallicColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+	float4 cEmissionColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+
+#ifdef _WITH_STANDARD_TEXTURE_MULTIPLE_DESCRIPTORS
+	if (gnTexturesMask & MATERIAL_ALBEDO_MAP) cAlbedoColor = gtxtAlbedoTexture.Sample(gssWrap, input.uv);
+	if (gnTexturesMask & MATERIAL_SPECULAR_MAP) cSpecularColor = gtxtSpecularTexture.Sample(gssWrap, input.uv);
+	if (gnTexturesMask & MATERIAL_NORMAL_MAP) cNormalColor = gtxtNormalTexture.Sample(gssWrap, input.uv);
+	if (gnTexturesMask & MATERIAL_METALLIC_MAP) cMetallicColor = gtxtMetallicTexture.Sample(gssWrap, input.uv);
+	if (gnTexturesMask & MATERIAL_EMISSION_MAP) cEmissionColor = gtxtEmissionTexture.Sample(gssWrap, input.uv);
+#else
+	if (gnTexturesMask & MATERIAL_ALBEDO_MAP) cAlbedoColor = gtxtStandardTextures[0].Sample(gssWrap, input.uv);
+	if (gnTexturesMask & MATERIAL_SPECULAR_MAP) cSpecularColor = gtxtStandardTextures[1].Sample(gssWrap, input.uv);
+	if (gnTexturesMask & MATERIAL_NORMAL_MAP) cNormalColor = gtxtStandardTextures[2].Sample(gssWrap, input.uv);
+	if (gnTexturesMask & MATERIAL_METALLIC_MAP) cMetallicColor = gtxtStandardTextures[3].Sample(gssWrap, input.uv);
+	if (gnTexturesMask & MATERIAL_EMISSION_MAP) cEmissionColor = gtxtStandardTextures[4].Sample(gssWrap, input.uv);
+#endif
+
+	float4 cIllumination = float4(1.0f, 1.0f, 1.0f, 1.0f);
+	float4 cColor = cAlbedoColor + cSpecularColor + cEmissionColor;
+	if (gnTexturesMask & MATERIAL_NORMAL_MAP)
+	{
+		float3 normalW = input.normalW;
+		float3x3 TBN = float3x3(normalize(input.tangentW), normalize(input.bitangentW), normalize(input.normalW));
+		float3 vNormal = normalize(cNormalColor.rgb * 2.0f - 1.0f); //[0, 1] ¡æ [-1, 1]
+		normalW = normalize(mul(vNormal, TBN));
+		cIllumination = Lighting(input.positionW, normalW);
+		cColor = lerp(cColor, cIllumination, 0.5f);
+	}
+
+	return(cColor);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+struct VS_SKYBOX_CUBEMAP_INPUT
+{
+	float3 position : POSITION;
+};
+
+struct VS_SKYBOX_CUBEMAP_OUTPUT
+{
+	float3	positionL : POSITION;
+	float4	position : SV_POSITION;
+};
+
+VS_SKYBOX_CUBEMAP_OUTPUT VSSkyBox(VS_SKYBOX_CUBEMAP_INPUT input)
+{
+	VS_SKYBOX_CUBEMAP_OUTPUT output;
+
+	output.position = mul(mul(mul(float4(input.position, 1.0f), gmtxGameObject), gmtxView), gmtxProjection);
+	output.positionL = input.position;
+
+	return(output);
+}
+
+TextureCube gtxtSkyCubeTexture : register(t13);
+SamplerState gssClamp : register(s1);
+
+float4 PSSkyBox(VS_SKYBOX_CUBEMAP_OUTPUT input) : SV_TARGET
+{
+	float4 cColor = gtxtSkyCubeTexture.Sample(gssClamp, input.positionL);
+
+	return(cColor);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+struct VS_SPRITE_TEXTURED_INPUT
+{
+	float3 position : POSITION;
+	float2 uv : TEXCOORD;
+};
+
+struct VS_SPRITE_TEXTURED_OUTPUT
+{
+	float4 position : SV_POSITION;
+	float2 uv : TEXCOORD;
+};
+
+VS_SPRITE_TEXTURED_OUTPUT VSTextured(VS_SPRITE_TEXTURED_INPUT input)
+{
+	VS_SPRITE_TEXTURED_OUTPUT output;
+
+	output.position = mul(mul(mul(float4(input.position, 1.0f), gmtxGameObject), gmtxView), gmtxProjection);
+	output.uv = input.uv;
+
+	return(output);
+}
+
+/*
+float4 PSTextured(VS_SPRITE_TEXTURED_OUTPUT input, uint nPrimitiveID : SV_PrimitiveID) : SV_TARGET
+{
+	float4 cColor;
+	if (nPrimitiveID < 2)
+		cColor = gtxtTextures[0].Sample(gWrapSamplerState, input.uv);
+	else if (nPrimitiveID < 4)
+		cColor = gtxtTextures[1].Sample(gWrapSamplerState, input.uv);
+	else if (nPrimitiveID < 6)
+		cColor = gtxtTextures[2].Sample(gWrapSamplerState, input.uv);
+	else if (nPrimitiveID < 8)
+		cColor = gtxtTextures[3].Sample(gWrapSamplerState, input.uv);
+	else if (nPrimitiveID < 10)
+		cColor = gtxtTextures[4].Sample(gWrapSamplerState, input.uv);
+	else
+		cColor = gtxtTextures[5].Sample(gWrapSamplerState, input.uv);
+	float4 cColor = gtxtTextures[NonUniformResourceIndex(nPrimitiveID/2)].Sample(gWrapSamplerState, input.uv);
+
+	return(cColor);
+}
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+Texture2D gtxtTerrainTexture : register(t14);
+Texture2D gtxtDetailTexture : register(t15);
+Texture2D gtxtAlphaTexture : register(t16);
+
+float4 PSTextured(VS_SPRITE_TEXTURED_OUTPUT input) : SV_TARGET
+{
+	float4 cColor = gtxtTerrainTexture.Sample(gssWrap, input.uv);
+
+	return(cColor);
+}
+
+struct VS_TERRAIN_INPUT
+{
+	float3 position : POSITION;
+	float4 color : COLOR;
+	float2 uv0 : TEXCOORD0;
+	float2 uv1 : TEXCOORD1;
+};
+
+struct VS_TERRAIN_OUTPUT
+{
+	float4 position : SV_POSITION;
+	float4 color : COLOR;
+	float2 uv0 : TEXCOORD0;
+	float2 uv1 : TEXCOORD1;
+};
+
+VS_TERRAIN_OUTPUT VSTerrain(VS_TERRAIN_INPUT input)
+{
+	VS_TERRAIN_OUTPUT output;
+
+	output.position = mul(mul(mul(float4(input.position, 1.0f), gmtxGameObject), gmtxView), gmtxProjection);
+	output.color = input.color;
+	output.uv0 = input.uv0;
+	output.uv1 = input.uv1;
+
+	return(output);
+}
+
+float4 PSTerrain(VS_TERRAIN_OUTPUT input) : SV_TARGET
+{
+	//float4 cIllumination = float4(1.0f, 1.0f, 1.0f, 1.0f);
+	//float3 normalW = float4(0.0f, 1.0f, 0.0f, 0.0f);
+	//cIllumination = Lighting(input.position, normalW);
+
+	float4 cBaseTexColor = gtxtTerrainTexture.Sample(gssWrap, input.uv0);
+	float4 cDetailTexColor = gtxtDetailTexture.Sample(gssWrap, input.uv1);
+	//	float fAlpha = gtxtTerrainTexture.Sample(gssWrap, input.uv0);
+
+	float4 cColor = cBaseTexColor * 0.5f + cDetailTexColor * 0.5f;
+	//float4 cColor = saturate(lerp(cBaseTexColor, cDetailTexColor, fAlpha));
+	//float4 cColor = saturate((cIllumination) * 0.5f + (cBaseTexColor + cDetailTexColor) * 0.5f);
+
+
+	return(cColor);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+struct VS_BILLBOARD_INSTANCING_INPUT
+{
+	float3 position : POSITION;
+	float2 uv : TEXCOORD;
+	float3 instancePosition : INSTANCEPOSITION;
+	float4 billboardInfo : BILLBOARDINFO; //(cx, cy, type, texture)
+};
+
+struct VS_BILLBOARD_INSTANCING_OUTPUT
+{
+	float4 position : SV_POSITION;
+	float2 uv : TEXCOORD;
+	int textureID : TEXTUREID;
+};
+
+//#define _WITH_BILLBOARD_ANIMATION
+
+VS_BILLBOARD_INSTANCING_OUTPUT VSBillboardInstancing(VS_BILLBOARD_INSTANCING_INPUT input)
+{
+	VS_BILLBOARD_INSTANCING_OUTPUT output;
+
+	input.position.x *= (input.billboardInfo.x * 0.5f);
+	input.position.y *= (input.billboardInfo.y * 0.5f);
+
+	float3 f3Look = normalize(gvCameraPosition - input.instancePosition);
+	float3 f3Up = float3(0.0f, 1.0f, 0.0f);
+	float3 f3Right = normalize(cross(f3Up, f3Look));
+
+	matrix mtxWorld;
+	mtxWorld[0] = float4(f3Right, 0.0f);
+	mtxWorld[1] = float4(f3Up, 0.0f);
+	mtxWorld[2] = float4(f3Look, 0.0f);
+	mtxWorld[3] = float4(input.instancePosition, 1.0f);
+
+	output.position = mul(mul(mul(float4(input.position, 1.0f), mtxWorld), gmtxView), gmtxProjection);
+
+#ifdef _WITH_BILLBOARD_ANIMATION
+	if (input.uv.y < 0.7f)
+	{
+		float fShift = 0.0f;
+		uint nResidual = ((uint)gfCurrentTime % 4);
+		if (nResidual == 1) fShift = -gfElapsedTime * 10.5f;
+		if (nResidual == 3) fShift = +gfElapsedTime * 10.5f;
+		input.uv.x += fShift;
+	}
+#endif
+	output.uv = input.uv;
+
+	output.textureID = (int)input.billboardInfo.w - 1;
+
+	return(output);
+}
+
+Texture2D<float4> gtxtBillboardTextures[7] : register(t17);
+
+
+
+float4 PSBillboardInstancing(VS_BILLBOARD_INSTANCING_OUTPUT input) : SV_TARGET
+{
+	float4 cColor = gtxtBillboardTextures[NonUniformResourceIndex(input.textureID)].Sample(gssWrap, input.uv);
+
+
+
+	return(cColor);
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//Ãß°¡
+
+struct VS_WATER_INPUT
+{
+	float3 position : POSITION;
+	float2 uv : TEXCOORD0;
+};
+
+struct VS_WATER_OUTPUT
+{
+	float4 position : SV_POSITION;
+	float2 uv : TEXCOORD0;
+};
+
+VS_WATER_OUTPUT VSTerrainWater(VS_WATER_INPUT input)
+{
+	VS_WATER_OUTPUT output;
+
+	output.position = mul(mul(mul(float4(input.position, 1.0f), gmtxGameObject), gmtxView), gmtxProjection);
+	output.uv = input.uv;
+
+	return(output);
+}
+
+Texture2D<float4> gtxtWaterBaseTexture : register(t24);
+Texture2D<float4> gtxtWaterDetail0Texture : register(t25);
+Texture2D<float4> gtxtWaterDetail1Texture : register(t26);
+
+static matrix<float, 3, 3> sf3x3TextureAnimation = { { 1.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f, 0.0f } };
+
+#define _WITH_TEXTURE_ANIMATION
+
+//#define _WITH_BASE_TEXTURE_ONLY
+#define _WITH_FULL_TEXTURES
+
+
+
+#ifndef _WITH_TEXTURE_ANIMATION
+float4 PSTerrainWater(VS_WATER_OUTPUT input) : SV_TARGET
+{
+	float4 cBaseTexColor = gtxtWaterBaseTexture.Sample(gssWrap, input.uv);
+	float4 cDetail0TexColor = gtxtWaterDetail0Texture.Sample(gssWrap, input.uv * 20.0f);
+	float4 cDetail1TexColor = gtxtWaterDetail1Texture.Sample(gssWrap, input.uv * 20.0f);
+
+	float4 cColor = float4(0.0f, 0.0f, 0.0f, 0.0f);
+#ifdef _WITH_BASE_TEXTURE_ONLY
+	cColor = cBaseTexColor;
+#else
+#ifdef _WITH_FULL_TEXTURES
+	cColor = lerp(cBaseTexColor * cDetail0TexColor, cDetail1TexColor.r * 0.5f, 0.35f);
+#else
+	cColor = cBaseTexColor * cDetail0TexColor;
+#endif
+#endif
+	return(cColor);
+}
+#else
+#define _WITH_CONSTANT_BUFFER_MATRIX
+//#define _WITH_STATIC_MATRIX
+
+float4 PSTerrainWater(VS_WATER_OUTPUT input) : SV_TARGET
+{
+	float2 uv = input.uv;
+
+#ifdef _WITH_STATIC_MATRIX
+	sf3x3TextureAnimation._m21 = gfCurrentTime * 0.00125f;
+	uv = mul(float3(input.uv, 1.0f), sf3x3TextureAnimation).xy;
+#else
+#ifdef _WITH_CONSTANT_BUFFER_MATRIX
+	uv = mul(float3(input.uv, 1.0f), (float3x3)gf4x4TextureAnimation).xy;
+	//	uv = mul(float4(uv, 1.0f, 0.0f), gf4x4TextureAnimation).xy;
+#else
+	uv.y += gfCurrentTime * 0.00125f;
+#endif
+#endif
+
+	float4 cBaseTexColor = gtxtWaterBaseTexture.SampleLevel(gssWrap, uv, 0);
+	float4 cDetail0TexColor = gtxtWaterDetail0Texture.SampleLevel(gssWrap, uv * 20.0f, 0);
+	float4 cDetail1TexColor = gtxtWaterDetail1Texture.SampleLevel(gssWrap, uv * 20.0f, 0);
+
+	float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+#ifdef _WITH_BASE_TEXTURE_ONLY
+	cColor = cBaseTexColor;
+#else
+#ifdef _WITH_FULL_TEXTURES
+	cColor = lerp(cBaseTexColor * cDetail0TexColor, cDetail1TexColor.r * 0.5f, 0.35f);
+#else
+	cColor = cBaseTexColor * cDetail0TexColor;
+#endif
+#endif
+
+	return(cColor);
+}
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//fog
+
+Texture2D<float4> gtxFogTexture : register(t27);
+
+struct VS_FOG_INPUT
+{
+	float3 position : POSITION;
+	float2 uv : TEXCOORD0;
+};
+
+struct VS_FOG_OUTPUT
+{
+	float4 position : SV_POSITION;
+	float2 uv : TEXCOORD0;
+};
+
+float4 FogColor = { 0.7f, 0.7f, 0.7f, 1.0f };
+float FogStart = 5.0f;
+float FogRange = 150.0f;
+
+VS_FOG_OUTPUT VSFog(VS_FOG_INPUT input)
+{
+	VS_FOG_OUTPUT output;
+
+	output.position = mul(mul(mul(float4(input.position, 1.0f), gmtxGameObject), gmtxView), gmtxProjection);
+	output.uv = input.uv;
+
+	return(output);
+}
+
+float4 PSFog(VS_FOG_OUTPUT input) : SV_TARGET
+{
+
+	//float4 diffuseAlbedo = cColor;
+	//float3 toEyeW = gvCameraPosition - input.position;
+	//float distToEye = length(toEyeW);
+	////////toEyeW /= distToEye; // normalize
+	//float fogAmount = saturate((distToEye - FogStart) / FogRange);
+	//cColor = lerp(cColor, FogColor, fogAmount);
+	float2 uv = input.uv;
+
+	float4 cBaseTexColor = gtxFogTexture.SampleLevel(gssWrap, uv, 0);
+	float4 cColor = float4(0.0f, 0.0f, 0.0f, 0.5f);
+	cColor = cBaseTexColor;
+	if (cColor.x == 0.f && cColor.y == 0.f && cColor.z == 0.f);
+	else
+		cColor.w = 0.5f;
+	return(cColor);
+}
---
+++ UILayer.cpp
@@ -0,0 +1,264 @@
+﻿#include "stdafx.h"
+#include "UILayer.h"
+
+using namespace std;
+
+UILayer::UILayer(UINT nFrames, UINT nTextBlocks, ID3D12Device* pd3dDevice, ID3D12CommandQueue* pd3dCommandQueue, ID3D12Resource** ppd3dRenderTargets, UINT nWidth, UINT nHeight)
+{
+    m_fWidth = static_cast<float>(nWidth);
+    m_fHeight = static_cast<float>(nHeight);
+    m_nRenderTargets = nFrames;
+    m_ppd3d11WrappedRenderTargets = new ID3D11Resource*[nFrames];
+    m_ppd2dRenderTargets = new ID2D1Bitmap1*[nFrames];
+
+    m_nTextBlocks = nTextBlocks;
+    m_pTextBlocks = new TextBlock[nTextBlocks];
+    m_ppoint1 = { FRAME_BUFFER_WIDTH - 70.f ,10.f };
+    for (int i = 0; i < OBJNUM; ++i)
+    {
+        bShowObj[i] = false;
+    }
+
+    InitializeDevice(pd3dDevice, pd3dCommandQueue, ppd3dRenderTargets);
+}
+
+void UILayer::InitializeDevice(ID3D12Device* pd3dDevice, ID3D12CommandQueue* pd3dCommandQueue, ID3D12Resource** ppd3dRenderTargets)
+{
+    UINT d3d11DeviceFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
+    D2D1_FACTORY_OPTIONS d2dFactoryOptions = { };
+
+#if defined(_DEBUG) || defined(DBG)
+    d2dFactoryOptions.debugLevel = D2D1_DEBUG_LEVEL_INFORMATION;
+    d3d11DeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+    ID3D11Device* pd3d11Device = NULL;
+    ID3D12CommandQueue* ppd3dCommandQueues[] = { pd3dCommandQueue };
+    ::D3D11On12CreateDevice(pd3dDevice, d3d11DeviceFlags, nullptr, 0, reinterpret_cast<IUnknown**>(ppd3dCommandQueues), _countof(ppd3dCommandQueues), 0, (ID3D11Device **)&pd3d11Device, (ID3D11DeviceContext **)&m_pd3d11DeviceContext, nullptr);
+
+    pd3d11Device->QueryInterface(__uuidof(ID3D11On12Device), (void **)&m_pd3d11On12Device);
+    pd3d11Device->Release();
+
+#if defined(_DEBUG) || defined(DBG)
+    ID3D12InfoQueue* pd3dInfoQueue;
+    if (SUCCEEDED(pd3dDevice->QueryInterface(IID_PPV_ARGS(&pd3dInfoQueue))))
+    {
+        D3D12_MESSAGE_SEVERITY pd3dSeverities[] = { D3D12_MESSAGE_SEVERITY_INFO };
+        D3D12_MESSAGE_ID pd3dDenyIds[] = { D3D12_MESSAGE_ID_INVALID_DESCRIPTOR_HANDLE };
+
+        D3D12_INFO_QUEUE_FILTER d3dInforQueueFilter = { };
+        d3dInforQueueFilter.DenyList.NumSeverities = _countof(pd3dSeverities);
+        d3dInforQueueFilter.DenyList.pSeverityList = pd3dSeverities;
+        d3dInforQueueFilter.DenyList.NumIDs = _countof(pd3dDenyIds);
+        d3dInforQueueFilter.DenyList.pIDList = pd3dDenyIds;
+
+        pd3dInfoQueue->PushStorageFilter(&d3dInforQueueFilter);
+    }
+    pd3dInfoQueue->Release();
+#endif
+
+    IDXGIDevice* pdxgiDevice = NULL;
+    m_pd3d11On12Device->QueryInterface(__uuidof(IDXGIDevice), (void **)&pdxgiDevice);
+
+    ::D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, __uuidof(ID2D1Factory3), &d2dFactoryOptions, (void **)&m_pd2dFactory);
+    HRESULT hResult = m_pd2dFactory->CreateDevice(pdxgiDevice, (ID2D1Device2 **)&m_pd2dDevice);
+    m_pd2dDevice->CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_NONE, (ID2D1DeviceContext2 **)&m_pd2dDeviceContext);
+
+    m_pd2dDeviceContext->SetTextAntialiasMode(D2D1_TEXT_ANTIALIAS_MODE_GRAYSCALE);
+
+    ::DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED, __uuidof(IDWriteFactory), (IUnknown **)&m_pd2dWriteFactory);
+    pdxgiDevice->Release();
+
+    D2D1_BITMAP_PROPERTIES1 d2dBitmapProperties = D2D1::BitmapProperties1(D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW, D2D1::PixelFormat(DXGI_FORMAT_UNKNOWN, D2D1_ALPHA_MODE_PREMULTIPLIED));
+
+    for (UINT i = 0; i < m_nRenderTargets; i++)
+    {
+        D3D11_RESOURCE_FLAGS d3d11Flags = { D3D11_BIND_RENDER_TARGET };
+        m_pd3d11On12Device->CreateWrappedResource(ppd3dRenderTargets[i], &d3d11Flags, D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT, IID_PPV_ARGS(&m_ppd3d11WrappedRenderTargets[i]));
+        IDXGISurface* pdxgiSurface = NULL;
+        m_ppd3d11WrappedRenderTargets[i]->QueryInterface(__uuidof(IDXGISurface), (void**)&pdxgiSurface);
+        m_pd2dDeviceContext->CreateBitmapFromDxgiSurface(pdxgiSurface, &d2dBitmapProperties, &m_ppd2dRenderTargets[i]);
+        pdxgiSurface->Release();
+    }
+}
+
+ID2D1SolidColorBrush* UILayer::CreateBrush(D2D1::ColorF d2dColor)
+{
+    ID2D1SolidColorBrush* pd2dDefaultTextBrush = NULL;
+    m_pd2dDeviceContext->CreateSolidColorBrush(d2dColor, &pd2dDefaultTextBrush);
+
+    return(pd2dDefaultTextBrush);
+}
+
+IDWriteTextFormat* UILayer::CreateTextFormat(WCHAR* pszFontName, float fFontSize)
+{
+    IDWriteTextFormat* pdwDefaultTextFormat = NULL;
+    m_pd2dWriteFactory->CreateTextFormat(L"궁서체", nullptr, DWRITE_FONT_WEIGHT_NORMAL, DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_STRETCH_NORMAL, fFontSize, L"en-us", &pdwDefaultTextFormat);
+
+    pdwDefaultTextFormat->SetTextAlignment(DWRITE_TEXT_ALIGNMENT_CENTER);
+    pdwDefaultTextFormat->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_NEAR);
+    //m_pd2dWriteFactory->CreateTextFormat(L"Arial", nullptr, DWRITE_FONT_WEIGHT_NORMAL, DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_STRETCH_NORMAL, fSmallFontSize, L"en-us", &m_pdwDefaultTextFormat);
+
+    return(pdwDefaultTextFormat);
+}
+
+void UILayer::UpdateTextOutputs(UINT nIndex, WCHAR* pstrUIText, D2D1_RECT_F* pd2dLayoutRect, IDWriteTextFormat* pdwFormat, ID2D1SolidColorBrush* pd2dTextBrush)
+{
+    //wcscpy_s(m_pTextBlocks[nIndex].m_pstrText, 256, pstrUIText);
+    if (pstrUIText) wcscpy_s(m_pTextBlocks[nIndex].m_pstrText, 256, pstrUIText);
+    if (pd2dLayoutRect) m_pTextBlocks[nIndex].m_d2dLayoutRect = *pd2dLayoutRect;
+    if (pdwFormat) m_pTextBlocks[nIndex].m_pdwFormat = pdwFormat;
+    if (pd2dTextBrush) m_pTextBlocks[nIndex].m_pd2dTextBrush = pd2dTextBrush;
+}
+
+void UILayer::Render(UINT nFrame)
+{
+    ID3D11Resource* ppResources[] = { m_ppd3d11WrappedRenderTargets[nFrame] };
+
+    m_pd2dDeviceContext->SetTarget(m_ppd2dRenderTargets[nFrame]);
+    m_pd3d11On12Device->AcquireWrappedResources(ppResources, _countof(ppResources));
+
+    //Make circle
+    float fBigRadius = 60.f;
+    D2D1_POINT_2F d2dcenter;
+    d2dcenter.x = FRAME_BUFFER_WIDTH - 70.f;
+    d2dcenter.y = 70.f;
+
+    D2D1_ELLIPSE d2dElipse0 = D2D1::Ellipse(d2dcenter, 30.0f, 30.0f);
+    D2D1_ELLIPSE d2dElipse1 = D2D1::Ellipse(d2dcenter, 15.0f, 15.0f);
+    D2D1_ELLIPSE d2dElipse2 = D2D1::Ellipse(d2dcenter, 30.0f, 30.0f);
+    D2D1_ELLIPSE d2dElipse3 = D2D1::Ellipse(d2dcenter, 45.0f, 45.0f);
+    D2D1_ELLIPSE d2dElipse4 = D2D1::Ellipse(d2dcenter, 60.0f, 60.0f);
+    D2D1_ELLIPSE d2dElipsePlayer = D2D1::Ellipse(d2dcenter, 2.0f, 2.0f);
+    //Make Time Rect
+    D2D1_RECT_F d2dRect = D2D1::RectF(30.0f, 20.0f, 70.f, 40.f);
+    D2D1_ROUNDED_RECT d2dRect1 = D2D1::RoundedRect(d2dRect, 0.1f, 0.1f);
+    //Make Line
+    D2D1_POINT_2F d2dpoint0 = d2dcenter;
+
+    //Make Hp Rect
+    D2D1_RECT_F d2dRectHp1 = D2D1::RectF(FRAME_BUFFER_WIDTH/2-150.f, 20.0f, FRAME_BUFFER_WIDTH/2 + 150.f - m_fpHpControl, 30.f);
+    D2D1_RECT_F d2dRectHp2 = D2D1::RectF(FRAME_BUFFER_WIDTH / 2 + 150.f - m_fpHpControl, 20.0f, FRAME_BUFFER_WIDTH / 2 + 150.f, 30.f);
+
+    ID2D1SolidColorBrush* pd2dBrushElipse0 = CreateBrush(D2D1::ColorF(D2D1::ColorF::Green, 1.0f));
+    ID2D1SolidColorBrush* pd2dBrush = CreateBrush(D2D1::ColorF(D2D1::ColorF::LawnGreen, 1.0f));
+    ID2D1SolidColorBrush* pd2dBrushRect = CreateBrush(D2D1::ColorF(D2D1::ColorF::Black, 0.4f));
+    ID2D1SolidColorBrush* pd2dBrushHpRect = CreateBrush(D2D1::ColorF(D2D1::ColorF::Red, 1.0f));
+    ID2D1SolidColorBrush* pd2dBrushHp2Rect = CreateBrush(D2D1::ColorF(D2D1::ColorF::Red, 0.5f));
+    ID2D1SolidColorBrush* pd2dBrushElipseObj = CreateBrush(D2D1::ColorF(D2D1::ColorF::Red, 1.0f));
+
+
+    //rader caculate
+    if (m_ppoint1.x >= d2dcenter.x + fBigRadius)
+        m_fPlusRidius *= -1;
+    else if (m_ppoint1.x <= d2dcenter.x - fBigRadius)
+        m_fPlusRidius *= -1;
+
+    m_ppoint1.x += m_fPlusRidius;
+    if (m_ppoint1.x < d2dcenter.x)
+        m_fPlusRidius *= 1;
+    if(m_ppoint1.x - d2dcenter.x != 0.f)
+        m_ppoint1.y = -m_fPlusRidius * sqrtf(((fBigRadius * fBigRadius) - ((m_ppoint1.x - d2dcenter.x) * (m_ppoint1.x - d2dcenter.x)))) + d2dcenter.y;
+    if (m_fPlusRidius == 1 && (m_ppoint1.y < d2dpoint0.y && m_ppoint1.x > d2dpoint0.x) && bUpdate)
+    {
+        UpdateRader();
+        bUpdate = false;
+    }
+    else if(m_fPlusRidius == -1 && (m_ppoint1.y > d2dpoint0.y && m_ppoint1.x < d2dpoint0.x) && !bUpdate)
+        bUpdate = true;
+
+
+    m_pd2dDeviceContext->BeginDraw();
+
+    //m_pd2dDeviceContext->DrawRectangle(d2dRect, pd2dBrushRect, 50.0f, NULL);
+    m_pd2dDeviceContext->DrawRoundedRectangle(d2dRect1, pd2dBrushRect, 50.0f, NULL);
+    for (UINT i = 0; i < m_nTextBlocks; i++)
+    {
+        m_pd2dDeviceContext->DrawText(m_pTextBlocks[i].m_pstrText, (UINT)wcslen(m_pTextBlocks[i].m_pstrText), m_pTextBlocks[i].m_pdwFormat, m_pTextBlocks[i].m_d2dLayoutRect, m_pTextBlocks[i].m_pd2dTextBrush);
+    }
+    m_pd2dDeviceContext->DrawEllipse(d2dElipse0, pd2dBrushElipse0, 60.0f, NULL);
+    m_pd2dDeviceContext->DrawEllipse(d2dElipse1, pd2dBrush, 1.0f, NULL);
+    m_pd2dDeviceContext->DrawEllipse(d2dElipse2, pd2dBrush, 1.0f, NULL);
+    m_pd2dDeviceContext->DrawEllipse(d2dElipse3, pd2dBrush, 1.0f, NULL);
+    m_pd2dDeviceContext->DrawEllipse(d2dElipse4, pd2dBrush, 1.0f, NULL);
+    m_pd2dDeviceContext->DrawEllipse(d2dElipsePlayer, pd2dBrush, 4.0f, NULL);
+    m_pd2dDeviceContext->DrawLine(d2dpoint0, m_ppoint1, pd2dBrush, 1.0f, NULL);
+    m_pd2dDeviceContext->DrawRectangle(d2dRectHp1, pd2dBrushHpRect, 10.0f, NULL);
+    m_pd2dDeviceContext->DrawRectangle(d2dRectHp2, pd2dBrushHp2Rect, 10.0f, NULL);
+    for (int i = 0; i < OBJNUM; ++i)
+    {
+        if (bShowObj[i])
+        {
+            m_pd2dDeviceContext->DrawEllipse(ObjEllipse[i], pd2dBrushElipseObj, 2.0f, NULL);
+        }
+    }
+
+    m_pd2dDeviceContext->EndDraw();
+
+    m_pd3d11On12Device->ReleaseWrappedResources(ppResources, _countof(ppResources));
+    m_pd3d11DeviceContext->Flush();
+}
+
+void UILayer::ReleaseResources()
+{
+    for (UINT i = 0; i < m_nTextBlocks; i++)
+    {
+        m_pTextBlocks[i].m_pdwFormat->Release();
+        m_pTextBlocks[i].m_pd2dTextBrush->Release();
+    }
+
+    for (UINT i = 0; i < m_nRenderTargets; i++)
+    {
+        ID3D11Resource* ppResources[] = { m_ppd3d11WrappedRenderTargets[i] };
+        m_pd3d11On12Device->ReleaseWrappedResources(ppResources, _countof(ppResources));
+    }
+
+    m_pd2dDeviceContext->SetTarget(nullptr);
+    m_pd3d11DeviceContext->Flush();
+
+    for (UINT i = 0; i < m_nRenderTargets; i++)
+    {
+        m_ppd2dRenderTargets[i]->Release();
+        m_ppd3d11WrappedRenderTargets[i]->Release();
+    }
+
+    m_pd2dDeviceContext->Release();
+    m_pd2dWriteFactory->Release();
+    m_pd2dDevice->Release();
+    m_pd2dFactory->Release();
+    m_pd3d11DeviceContext->Release();
+    m_pd3d11On12Device->Release();
+}
+
+void UILayer::UpdateRader()
+{
+    D2D1_POINT_2F d2dcenter;
+    d2dcenter.x = FRAME_BUFFER_WIDTH - 70.f;
+    d2dcenter.y = 70.f;
+
+    
+    for (int i = 0; i < OBJNUM; ++i)
+    {
+        bShowObj[i] = false;
+    }
+
+    for (int i = 0; i < OBJNUM; ++i)
+    {
+        float distX = PlayerPosition.x - ObjPosition[i].x;
+        float distZ = PlayerPosition.z - ObjPosition[i].z;
+        if (sqrtf((distX) * (distX) + (distZ) * (distZ)) <= 1000.f)
+        {
+            bShowObj[i] = true;
+        }
+
+        if (bShowObj[i])
+        {
+            D2D1_POINT_2F point = d2dcenter;
+            point.x -= distX * 60 / 1000;
+            point.y += distZ * 60 / 1000; //좌표계 반대
+            ObjEllipse[i] = D2D1::Ellipse(point, 1.0f, 1.0f);
+        }
+    }
+
+}
+
